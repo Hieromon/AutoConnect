d8c1223d6f393ea0f49c3a9ef8f93c441b371dc4
Hieromon Ikasamo <Hieromon@users.noreply.github.com>
2020-04-19 04:32:09 +0900

    Fixed Apply button not work #206

--- src/AutoConnectPage.cpp
+++ src/AutoConnectPage.cpp
@@ -706,7 +706,7 @@ const char  AutoConnect::_PAGE_CONFIGNEW[] PROGMEM = {
       "document.getElementById('ssid').value=e,document.getElementById('passphrase').focus()"
     "}"
     "function vsw(e){"
-      "var t;t=e?'none':'table-row';for(const e of document.getElementsByClassName('exp'))e.style.display=t;e||document.getElementById('sip').focus()"
+      "var t;t=e?'none':'table-row';for(const n of document.getElementsByClassName('exp'))n.style.display=t,n.getElementsByTagName('input')[0].disabled=e;e||document.getElementById('sip').focus()"
     "}"
   "</script>"
   "</body>"
